ceea29afa2f892fc7729870a8d1b23d2f916bc88
mohankossie <mohankossie@gmail.com>
2022-11-26 09:13:50 +0200

    My third commit

--- myworld/settings.py
+++ myworld/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@#y=*%&mis(a=qgzgd$%ol&9^0nt)f%r0cp%f=!upg1m^%u%ff'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
